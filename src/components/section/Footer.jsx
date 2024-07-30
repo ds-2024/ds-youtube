bf1a3279fc616fdf5c5a11fa2380b17e68224be5
Sophie1106 <yoonds714@nate.com>
2024-07-30 16:31:40 +0900

    11-메인컨텐츠작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer id='footer' role="contentinfo"> 
-        Footer
+        <a href='mailto:yoonds1106@gmail.com' rel='noopenner noreferrer'>
+          yoonds1106@gmail.com
+        </a>  
     </footer>
 
   )
